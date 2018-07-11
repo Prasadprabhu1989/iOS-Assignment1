333ceb2ecf9aec0f08e36ace0f988ccdea4cf055
Prasadprabhu1989 <prasadprabhu1989@gmail.com>
2018-07-11 11:16:19 +0530

    added testcase

--- iOS Assignment1/iOS Assignment1Tests/iOS_Assignment1Tests.swift
+++ iOS Assignment1/iOS Assignment1Tests/iOS_Assignment1Tests.swift
@@ -10,12 +10,17 @@ import XCTest
 @testable import iOS_Assignment1
 
 class iOS_Assignment1Tests: XCTestCase {
-    
+    let listViewController = ViewController()
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+   
+    func test_count_nil_check() {
+        listViewController.viewModel.fetchData(listFeed: .listFeed) {  results in
+        XCTAssertNotNil(results, "The object is Not Nil")
+        }
+    }
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
